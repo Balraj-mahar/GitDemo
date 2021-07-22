773e1e73adbf31375d7f53b5da518ed8d7b1a798
Balraj-mahar <balraj.mahar@globallogic.com>
2021-07-22 15:30:41 +0530

    Nunit class is not running

---
+++ FirstProject1/FirstProject1/Nunit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace FirstProject1
+{
+    [TestFixture]
+    class Nunit
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Intilize()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Manage().Window.Maximize();
+
+
+        }
+
+        [Test]
+        public void SearchTest()
+        {
+            driver.FindElement(By.Name("q")).SendKeys("c# with selenium");
+            driver.FindElement(By.XPath("(//input[@type='submit'])[3]")).Click();
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
---
+++ FirstProject1/FirstProject1/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace FirstProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("q")).SendKeys("c# with selenium");
+            driver.FindElement(By.XPath("(//input[@type='submit'])[3]")).Click();
+            driver.Close();
+        }
+    }
+}
